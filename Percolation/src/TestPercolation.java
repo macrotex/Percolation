101ae544c89ad737ec302c573ed553c1bcb9042a
Fred A. Habster <github238@macrotex.net>
2014-06-16 16:01:49 -0700

    More changes.

--- Percolation/src/TestPercolation.java
+++ Percolation/src/TestPercolation.java
@@ -4,20 +4,66 @@ import org.junit.Test;
 
 import coursera.adamhl.percolation.Percolation;
 
-
 public class TestPercolation {
 
 	@Test
-	public void PercolationTests() {
+	public void PercolationTestsBasic() {
 		// Create a Percolation object.
 		Percolation percolation = new Percolation(10) ;
 		assertTrue(percolation instanceof coursera.adamhl.percolation.Percolation) ;
 		
 		// Check that some of the elements in the initialized array are BLOCKED.
-		assertEquals(percolation.grid[0][0], Percolation.BLOCKED) ;
-		assertEquals(percolation.grid[2][3], Percolation.BLOCKED) ;
+		assertEquals(percolation.grid[0][0], Percolation.FULL) ;
+		assertEquals(percolation.grid[2][3], Percolation.FULL) ;
 		
-		assertTrue(true) ;
+		// Check the is_blocked method
+		assertTrue(percolation.isFull(1, 1)) ;
+		assertTrue(percolation.isFull(2, 3)) ;
+		assertTrue(percolation.isFull(10, 10)) ;
+		
+		assertFalse(percolation.isOpen(1, 1)) ;
+		
+	    percolation.open(1, 1);
+	    assertTrue(percolation.isOpen(1, 1)) ;
+	    
+	    // Does this system percolate? (NO!)
+	    //assertFalse(percolation.percolates()) ;
 	}
 
+	// Test for exceptions.
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException1() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(20, 1)) ;
+	}
+	
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException2() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(11, 1)) ;
+	}
+	
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException3() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(1, 20)) ;
+	}
+	
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException4() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(1, 11)) ;
+	}
+	
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException5() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(0, 6)) ;
+	}
+	
+	@Test(expected = java.lang.IndexOutOfBoundsException.class)
+	public void PercolationTestsException6() {
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation.isOpen(6, 0)) ;
+	}
 }
--- Percolation/src/coursera/adamhl/percolation/Percolation.java
+++ Percolation/src/coursera/adamhl/percolation/Percolation.java
@@ -5,24 +5,44 @@ public class Percolation {
 	public int[][] grid ;
 	
 	// There are two values that a grid element can have: "open" or "blocked"
-	public static final int BLOCKED = 0;
-	public static final int OPEN = 0;
+	public static final int FULL = 0;
+	public static final int OPEN = 1;
 	
 	public Percolation(int N)              // create N-by-N grid, with all sites blocked
 	{
 		// We want to initialize all entries to blocked.
 		grid = new int[N][N];
-		for(int row = 0; row < grid.length; row++)
+		for (int row = 0; row < grid.length; row++)
 		{
 			for (int col = 0; col < grid.length; col++) 
 			{
-				grid[row][col] = BLOCKED ;
+				grid[row][col] = FULL ;
 			}
 		}
 
 	}
-//    public void open(int i, int j)         // open site (row i, column j) if it is not already
-//	public boolean isOpen(int i, int j)    // is site (row i, column j) open?
-//    public boolean isFull(int i, int j)    // is site (row i, column j) full?
-//	public boolean percolates()            // does the system percolate?
+	
+	public boolean isFull(int i, int j) {
+		if (grid[i-1][j-1] == FULL) {
+			return true ;
+		} else {
+			return false ;
+		}
+	}
+
+	public boolean isOpen(int i, int j) {
+		return (!this.isFull(i, j)) ;
+	}
+	
+	// open site (row i, column j) if it is not already
+	public void open(int i, int j) {
+		if (this.isFull(i,j)) {
+			this.grid[i-1][j-1] = OPEN ;		
+		}
+	}
+	
+	// does the system percolate?
+	public boolean percolates()  {
+		return true ;
+	}
 }
