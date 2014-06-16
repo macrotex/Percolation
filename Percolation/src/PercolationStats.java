d1db16ed69e95287e46fe1f6e1b6f43f2c44119a
Fred A. Habster <github238@macrotex.net>
2014-06-16 13:25:57 -0700

    first commit

---
+++ Percolation/src/PercolationStats.java
@@ -0,0 +1,13 @@
+import coursera.adamhl.percolation.Percolation;
+
+
+public class PercolationStats {
+	public static void main()
+	{
+		System.out.println("Hello World!");
+		
+		// Create a new Percolation object
+		Percolation percolation = new Percolation(10) ;
+		
+	}
+}
---
+++ Percolation/src/TestPercolation.java
@@ -0,0 +1,21 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import coursera.adamhl.percolation.Percolation;
+
+
+public class TestPercolation {
+
+	@Test
+	public void PercolationTests() {
+		// Create a Percolation object.
+		Percolation percolation = new Percolation(10) ;
+		assertTrue(percolation instanceof coursera.adamhl.percolation.Percolation) ;
+		
+		// Check that some of the elements in the initialized array are BLOCKED.
+		assertEquals(percolation.grid[0][0], Percolation.BLOCKED) ;
+		assertEquals(percolation.grid[2][3], Percolation.BLOCKED) ;
+	}
+
+}
